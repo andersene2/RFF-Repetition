2b63e2d0b1e390b136856961029126a28fea3d1d
Eric Andersen <andersene2@mail.sacredheart.edu>
2023-07-14 10:19:36 -0400

    Update on App,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,25 @@ function App() {
 
   return (
     <>
+    <div className='formHead'>
+      <h2 className='registrant-title'>Registrant #
+      <span className='registrantNum'>1</span></h2>
+      <div >
+        <button type="button">Clear all fields</button>
+      </div>
+    </div>
     <button type="button">Sign In</button>
     <label>Login with your RunSignup account.</label>
     <br></br>
     <br></br>
     <div>
-      <label>Who are you registering: </label>
-      <input></input>
+      <label htmlFor='applicant'>Who are you registering: </label>
+      <select id='applicant' required>
+          <option value="me">Me, 18+</option>
+          <option value="other_adult">Other Adult, 18+</option>
+          <option value="minor">Minor, Under 18</option>
+      </select>
+
     </div>
     <br></br>
     <div>
