08f385ffd2d698c321762fc963f36cd1853b279c
Eric Andersen <andersene2@mail.sacredheart.edu>
2023-07-14 15:10:31 -0400

    Adding states

--- src/App.jsx
+++ src/App.jsx
@@ -36,95 +36,97 @@ function App() {
 
   return (
     <>
-    <div style={{ width:"620px", height:"700px", border:"1px solid gray"}}>
-      <div className='formHead'>
-        <h2 className='registrant-title'>Registrant #
-        <span className='registrantNum'>1</span></h2>
-        <div>
-          <button type="button">Clear all fields</button>
+      <div style={{ width: "620px", height: "700px", border: "1px solid gray" }}>
+        <div className='formHead'>
+          <h2 className='registrant-title'>Registrant #
+            <span className='registrantNum'>1</span></h2>
+          <div>
+            <button type="button" onClick={clearAllFields}>Clear all fields</button>
+          </div>
         </div>
-      </div>
-      <button type="button">Sign In</button>
-      <label>Login with your RunSignup account.</label>
-      <br></br>
-      <br></br>
-      <div>
-        <label htmlFor='applicant'>Who are you registering: </label>
-        <select id='applicant' required>
+        <button type="button">Sign In</button>
+        <label>Login with your RunSignup account.</label>
+        <br></br>
+        <br></br>
+        <div>
+          <label htmlFor='applicant'>Who are you registering: </label>
+          <select id='applicant' required>
             <option value="me">Me, 18+</option>
             <option value="other_adult">Other Adult, 18+</option>
             <option value="minor">Minor, Under 18</option>
-        </select>
+          </select>
 
-      </div>
-      <br></br>
-      <div  >
-        <label>First Name: </label>
-        <input></input>
-
-        <label>Last Name: </label>
-        <input></input>
-
+        </div>
         <br></br>
+        <div  >
+          <label>First Name: 
+          <input type="text" name="first_Name" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
+          </label>
+
+          <label>Last Name: 
+          <input type="text" name="last_Name" value={lastName} onChange={e => setLastName(e.target.value)}></input>
+          </label>
+
+          <br></br>
+          <br></br>
+
+          <label>Email Address</label>
+          <input></input>
+
+          <label>Confirm Email: </label>
+          <input></input>
+
+          <br></br>
+          <br></br>
+
+          <label>Create Password: </label>
+          <input></input>
+
+          <label>Confirm Password: </label>
+          <input></input>
+          <br></br>
+        </div>
         <br></br>
+        <div>
+          <label>Date of Birth: </label>
+          <input type="date"></input>
 
-        <label>Email Address</label>
-        <input></input>
-
-        <label>Confirm Email: </label>
-        <input></input>
-
-        <br></br>
-        <br></br>
-
-        <label>Create Password: </label>
-        <input></input>
-
-        <label>Confirm Password: </label>
-        <input></input>
-        <br></br>
-      </div>
-      <br></br>
-      <div>
-        <label>Date of Birth: </label>
-        <input type="date"></input>
-
-        <span>Male or Female: </span>
+          <span>Male or Female: </span>
           <input id="male" type="radio"
-          name="answer" value="male" required></input>
+            name="answer" value="male" required></input>
           <label>Male</label>
 
           <input id="female" type="radio"
-          name="answer" value="female" required></input>
+            name="answer" value="female" required></input>
           <label>Female</label>
           <br></br>
           <br></br>
           <label>Phone</label>
           <input></input>
-      </div>
-      <br></br>
-      <div>
-        <label>Street Address</label>
-        <input></input>
-
-        <label>Country</label>
-        <input></input>
-
+        </div>
         <br></br>
+        <div>
+          <label>Street Address</label>
+          <input></input>
 
-        <label>Zip Code</label>
-        <input></input>
+          <label>Country</label>
+          <input></input>
 
-        <label>City</label>
-        <input></input>
+          <br></br>
 
-        <br></br>
+          <label>Zip Code</label>
+          <input></input>
 
-        <label>State</label>
-        <input></input>
+          <label>City</label>
+          <input></input>
+
+          <br></br>
+
+          <label>State</label>
+          <input></input>
+        </div>
+
       </div>
-      
-    </div>
     </>
   )
 }
