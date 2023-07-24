49b73c1e3b23c5bdfd9d88b439fba0b12ffbefa4
Eric Andersen <andersene2@mail.sacredheart.edu>
2023-07-24 17:19:43 -0400

    finishing up the base of the form

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,15 @@ function App() {
   const [createPassword, setCreatePassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
   const [DOB, setDOB] = useState("")
-  const [gender, setGender] = useState("")
+  const [selectedOption, setSelectedOption] = useState('');
   const [phone, setPhone] = useState("")
   const [streetAddress, setStreetAddress] = useState("")
   const [country, setCountry] = useState("")
   const [zipCode, setZipCode] = useState("")
   const [city, setCity] = useState("")
   const [state, setState] = useState("")
+  const [raceOption, setRaceOption] = useState("");
+
 
   function clearAllFields() {
 
@@ -29,20 +31,23 @@ function App() {
     setCreatePassword("");
     setConfirmPassword("");
     setDOB("");
-    setSelectedOption('');
+    setSelectedOption(''); //state for gender 
     setPhone("");
     setStreetAddress("");
     setCountry("");
     setZipCode("");
     setCity("");
     setState("");
+    setRaceOption(""); // state for Race Choice
   }
 
-const [selectedOption, setSelectedOption] = useState('');
 
-const handleOptionChange = (event) => {
-  setSelectedOption(event.target.value);
-};
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+  const handleRaceChange = (event) => {
+    setRaceOption(event.target.value);
+  };
 
   return (
     <>
@@ -60,8 +65,8 @@ const handleOptionChange = (event) => {
         <br></br>
         <div>
           <label htmlFor='applicant'>Who are you registering: </label>
-          <select id='applicant' value={registering} 
-          onChange={e => setRegistering(e.target.value)} required>
+          <select id='applicant' value={registering}
+            onChange={e => setRegistering(e.target.value)} required>
             <option disabled={true} value="">-Choose option-</option>
 
             <option value="me">Me, 18+</option>
@@ -72,11 +77,11 @@ const handleOptionChange = (event) => {
         </div>
         <br></br>
         <div  >
-          <label>First Name:
+          <label>First Name: 
             <input type="text" name="first_Name" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
           </label>
 
-          <label>Last Name:
+          <label>Last Name: 
             <input type="text" name="last_Name" value={lastName}
               onChange={e => setLastName(e.target.value)}></input>
           </label>
@@ -84,12 +89,12 @@ const handleOptionChange = (event) => {
           <br></br>
           <br></br>
 
-          <label>Email Address
+          <label>Email Address: 
             <input type='text' name='email_address' value={emailAddress}
               onChange={e => setEmailAdress(e.target.value)}></input>
           </label>
 
-          <label>Confirm Email:
+          <label>Confirm Email: 
             <input type='text' name='confirm_email' value={confirmEmail}
               onChange={e => setConfirmEmail(e.target.value)}></input>
           </label>
@@ -97,12 +102,12 @@ const handleOptionChange = (event) => {
           <br></br>
           <br></br>
 
-          <label>Create Password:
+          <label>Create Password: 
             <input type='text' name='create_password' value={createPassword}
               onChange={e => setCreatePassword(e.target.value)}></input>
           </label>
 
-          <label>Confirm Password:
+          <label>Confirm Password: 
             <input type='text' name='confirm_password' value={confirmPassword}
               onChange={e => setConfirmPassword(e.target.value)}></input>
           </label>
@@ -111,7 +116,7 @@ const handleOptionChange = (event) => {
         </div>
         <br></br>
         <div>
-          <label>Date of Birth:
+          <label>Date of Birth: 
             <input type="date" name='d.o.b.' value={DOB}
               onChange={e => setDOB(e.target.value)}></input>
           </label>
@@ -119,7 +124,7 @@ const handleOptionChange = (event) => {
           <span>Gender: </span>
           <label>
             <input id="male" type="radio"
-              name="answer" value='male' 
+              name="answer" value='male'
               checked={selectedOption === 'male'}
               onChange={handleOptionChange} required></input>
             Male
@@ -128,7 +133,7 @@ const handleOptionChange = (event) => {
           <label>
             <input id="female" type="radio"
               name="answer" value='female'
-              checked={selectedOption === 'female'} 
+              checked={selectedOption === 'female'}
               onChange={handleOptionChange} required></input>
             Female
           </label>
@@ -136,33 +141,33 @@ const handleOptionChange = (event) => {
           <br></br>
           <br></br>
 
-          <label>Phone:</label>
-          <input  type='text' name='phone' value={phone}
-            onChange={e => setPhone(e.target.value)}></input>
+          <label>Phone: </label>
+          <input type='text' name='phone' value={phone}
+            onChange={e => setPhone(e.target.value)}></input> Format: ###-###-####
         </div>
         <br></br>
         <div>
-          <label>Street Address:</label>
+          <label>Street Address: </label>
           <input type='text' name='street_address' value={streetAddress}
             onChange={e => setStreetAddress(e.target.value)}></input>
 
-          <label>Country:</label>
+          <label>Country: </label>
           <input type='text' name='country' value={country}
             onChange={e => setCountry(e.target.value)}></input>
 
           <br></br>
 
-          <label>Zip Code:</label>
+          <label>Zip Code: </label>
           <input type='text' name='zipCode' value={zipCode}
             onChange={e => setZipCode(e.target.value)}></input>
 
-          <label>City:</label>
+          <label>City: </label>
           <input type='text' name='city' value={city}
             onChange={e => setCity(e.target.value)}></input>
 
           <br></br>
 
-          <label>State:</label>
+          <label>State: </label>
           <input type='text' name='state' value={state}
             onChange={e => setState(e.target.value)}></input>
         </div>
@@ -170,20 +175,28 @@ const handleOptionChange = (event) => {
           <h2>Choose Your Event</h2>
         </div>
         <div>
-          <input id="5K" type="radio"
-          name="race" value="5k" required></input>
-          <span>5K ($35.00 + $3.60 SignUp Fee)</span> 
-            <br></br>
-          <input id="5k-Virtual" type='radio'
-          name='race' value='5K-Virtual'required></input>
-          <span>5k Virtual ($35.00 + $3.60 SignUp Fee)</span>
+          <label>
+            <input id="5K" type="radio"
+              name="race" value="5K"
+              checked={raceOption === '5K'}
+              onChange={handleRaceChange} required></input>
+            5K ($35.00 + $3.60 SignUp Fee)
+          </label>
+          <br></br>
+          <label>
+            <input id="5K-Virtual" type='radio'
+              name='race' value='5K-Virtual'
+              checked={raceOption === '5K-Virtual'}
+              onChange={handleRaceChange} required></input>
+            5K Virtual ($35.00 + $3.60 SignUp Fee)
+          </label>
         </div>
         <br></br>
         <button>Add Another Registrant</button>
         <br></br>
         <button>Continue</button>
       </div>
-      
+
     </>
   )
 }
