318d747ab427f0b1fb8d0c1e2b3ccc8be28dc991
Eric Andersen <andersene2@mail.sacredheart.edu>
2023-07-25 13:44:01 -0400

    Adding validation. Next is phone/email.

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
   };
 
   function validateForm() {
-    
+
     if (firstName.length == 0) {
       alert('Invalid Form. First Name can not be empty.')
       return
@@ -125,180 +125,208 @@ function App() {
       alert('Invalid Form, 0 digit characters in password.')
       return
     }
-    if (countSpecialCharacters == 0 ) {
+    if (countSpecialCharacters == 0) {
       alert('Invalid Form, 0 special characters in password.')
       return
     }
-    alert('Form is valid')
+    if (emailAddress !== confirmEmail) {
+      alert('Invalid Form, emails fo not match.')
+      return
+    }
+    if (password !== confirmPassword) {
+      alert('Invalid Form, passwords do not match.')
+      return
+    }
   }
 
   return (
     <>
       <div style={{ width: "620px", height: "800px", border: "1px solid gray" }}>
         <form>
-        <div className='formHead'>
-          <h2 className='registrant-title'>Registrant #
-            <span className='registrantNum'>1</span></h2>
-          <div>
-            <button type="button" onClick={clearAllFields}>Clear all fields</button>
+          <div className='formHead'>
+            <h2 className='registrant-title'>Registrant #
+              <span className='registrantNum'>1</span></h2>
+            <div>
+              <button type="button" onClick={clearAllFields}>Clear all fields</button>
+            </div>
           </div>
-        </div>
-        <button type="button">Sign In</button>
-        <label>Login with your RunSignup account.</label>
-        <br></br>
-        <br></br>
-        <div>
-          <label htmlFor='applicant'>Who are you registering: </label>
-          <select id='applicant' value={registering}
-            onChange={e => setRegistering(e.target.value)} required>
-            <option disabled={true} value="">-Choose option-</option>
-
-            <option value="me">Me, 18+</option>
-            <option value="other_adult">Other Adult, 18+</option>
-            <option value="minor">Minor, Under 18</option>
-          </select>
-
-        </div>
-        <br></br>
-        <div  >
-          <label>First Name:
-            <input 
-              type='text'
-              name='first_Name'
-              value={firstName}
-              placeholder='Enter first name...'
-              onChange={e => setFirstName(e.target.value)}
-            />
-          </label>
-
-          <label>Last Name:
-            <input
-              type='text'
-              name='last_Name'
-              placeholder='Enter last name...'
-              value={lastName}
-              onChange={e => setLastName(e.target.value)}
-            />
-          </label>
-
+          <button type="button">Sign In</button>
+          <label>Login with your RunSignup account.</label>
           <br></br>
           <br></br>
+          <div>
+            <label htmlFor='applicant'>Who are you registering: </label>
+            <select id='applicant' value={registering}
+              onChange={e => setRegistering(e.target.value)} required>
+              <option disabled={true} value="">-Choose option-</option>
 
-          <label>Email Address:
+              <option value="me">Me, 18+</option>
+              <option value="other_adult">Other Adult, 18+</option>
+              <option value="minor">Minor, Under 18</option>
+            </select>
+
+          </div>
+          <br></br>
+          <div  >
+            <label>First Name:
+              <input
+                type='text'
+                name='first_Name'
+                value={firstName}
+                placeholder='Enter first name...'
+                onChange={e => setFirstName(e.target.value)}
+              />
+            </label>
+
+            <label>Last Name:
+              <input
+                type='text'
+                name='last_Name'
+                placeholder='Enter last name...'
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+              />
+            </label>
+
+            <br></br>
+            <br></br>
+
+            <label>Email Address:
+              <input
+                type='text'
+                name='email_address'
+                placeholder='Type your email...'
+                value={emailAddress}
+                onChange={e => setEmailAdress(e.target.value)}
+              />
+            </label>
+
+            <label>Confirm Email:
+              <input 
+                type='text' 
+                name='confirm_email' 
+                value={confirmEmail}
+                onChange={e => setConfirmEmail(e.target.value)}></input>
+            </label>
+
+            <br></br>
+            <br></br>
+
+            <label>Create Password:
+              <input type='password' name='create_password' value={password}
+                onChange={e => setPassword(e.target.value)}></input>
+            </label>
+
+            <label>Confirm Password:
+              <input type='password' name='confirm_password' value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}></input>
+            </label>
+
+            <br></br>
+          </div>
+          <br></br>
+          <div>
+            <label>Date of Birth:
+              <input type="date" name='d.o.b.' value={DOB}
+                onChange={e => setDOB(e.target.value)} required></input>
+            </label>
+
+            <span>Gender: </span>
+            <label>
+              <input id="male" type="radio"
+                name="answer" value='male'
+                checked={selectedOption === 'male'}
+                onChange={handleOptionChange} required></input>
+              Male
+            </label>
+
+            <label>
+              <input id="female" type="radio"
+                name="answer" value='female'
+                checked={selectedOption === 'female'}
+                onChange={handleOptionChange} required></input>
+              Female
+            </label>
+
+            <br></br>
+            <br></br>
+
+            <label>Phone: </label>
             <input 
               type='text' 
-              name='email_address'
-              placeholder='Type your email...'
-              value={emailAddress}
-              onChange={e => setEmailAdress(e.target.value)}
-            />
-          </label>
-
-          <label>Confirm Email:
-            <input type='text' name='confirm_email' value={confirmEmail}
-              onChange={e => setConfirmEmail(e.target.value)}></input>
-          </label>
-
-          <br></br>
-          <br></br>
-
-          <label>Create Password:
-            <input type='text' name='create_password' value={password}
-              onChange={e => setPassword(e.target.value)}></input>
-          </label>
-
-          <label>Confirm Password:
-            <input type='text' name='confirm_password' value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}></input>
-          </label>
-
-          <br></br>
-        </div>
-        <br></br>
-        <div>
-          <label>Date of Birth:
-            <input type="date" name='d.o.b.' value={DOB}
-              onChange={e => setDOB(e.target.value)}></input>
-          </label>
-
-          <span>Gender: </span>
-          <label>
-            <input id="male" type="radio"
-              name="answer" value='male'
-              checked={selectedOption === 'male'}
-              onChange={handleOptionChange} required></input>
-            Male
-          </label>
-
-          <label>
-            <input id="female" type="radio"
-              name="answer" value='female'
-              checked={selectedOption === 'female'}
-              onChange={handleOptionChange} required></input>
-            Female
-          </label>
-
-          <br></br>
-          <br></br>
-
-          <label>Phone: </label>
-          <input type='text' name='phone' value={phone}
-            onChange={e => setPhone(e.target.value)}></input> Format: ###-###-####
-        </div>
-        <br></br>
-        <div>
-          <label>Street Address: </label>
-          <input type='text' name='street_address' value={streetAddress}
-            onChange={e => setStreetAddress(e.target.value)}></input>
-
-          <label>Country: </label>
-          <input type='text' name='country' value={country}
-            onChange={e => setCountry(e.target.value)}></input>
+              name='phone' 
+              placeholder='###-###-####'
+              value={phone}
+              onChange={e => setPhone(e.target.value)} required></input> 
+          </div>
 
           <br></br>
 
-          <label>Zip Code: </label>
-          <input type='text' name='zipCode' value={zipCode}
-            onChange={e => setZipCode(e.target.value)}></input>
+          <div>
+            <label>Street Address: </label>
+            <input 
+              type='text' 
+              name='street_address' 
+              value={streetAddress}
+              onChange={e => setStreetAddress(e.target.value)} 
+              required></input>
 
-          <label>City: </label>
-          <input type='text' name='city' value={city}
-            onChange={e => setCity(e.target.value)}></input>
+            <label>Country: </label>
+            <input 
+              type='text' 
+              name='country' 
+              value={country}
+              onChange={e => setCountry(e.target.value)}
+              required></input>
 
+            <br></br>
+
+            <label>Zip Code: </label>
+            <input 
+              type='text' 
+              name='zipCode' 
+              value={zipCode}
+              onChange={e => setZipCode(e.target.value)}
+              required></input>
+
+            <label>City: </label>
+            <input type='text' name='city' value={city}
+              onChange={e => setCity(e.target.value)} required></input>
+
+            <br></br>
+
+            <label>State: </label>
+            <input type='text' name='state' value={state}
+              onChange={e => setState(e.target.value)} required></input>
+          </div>
+          <div>
+            <h2>Choose Your Event</h2>
+          </div>
+          <div>
+            <label>
+              <input id="5K" type="radio"
+                name="race" value="5K"
+                checked={raceOption === '5K'}
+                onChange={handleRaceChange} required></input>
+              5K ($35.00 + $3.60 SignUp Fee)
+            </label>
+            <br></br>
+            <label>
+              <input id="5K-Virtual" type='radio'
+                name='race' value='5K-Virtual'
+                checked={raceOption === '5K-Virtual'}
+                onChange={handleRaceChange} required></input>
+              5K Virtual ($35.00 + $3.60 SignUp Fee)
+            </label>
+          </div>
           <br></br>
-
-          <label>State: </label>
-          <input type='text' name='state' value={state}
-            onChange={e => setState(e.target.value)}></input>
-        </div>
-        <div>
-          <h2>Choose Your Event</h2>
-        </div>
-        <div>
-          <label>
-            <input id="5K" type="radio"
-              name="race" value="5K"
-              checked={raceOption === '5K'}
-              onChange={handleRaceChange} required></input>
-            5K ($35.00 + $3.60 SignUp Fee)
-          </label>
+          <button>Add Another Registrant</button>
           <br></br>
-          <label>
-            <input id="5K-Virtual" type='radio'
-              name='race' value='5K-Virtual'
-              checked={raceOption === '5K-Virtual'}
-              onChange={handleRaceChange} required></input>
-            5K Virtual ($35.00 + $3.60 SignUp Fee)
-          </label>
-        </div>
-        <br></br>
-        <button>Add Another Registrant</button>
-        <br></br>
-        <button 
-          type='submit'
-          onClick={() => {validateForm()}}>
+          <button
+            type='submit'
+            onClick={() => { validateForm() }}>
             Continue
-        </button>
+          </button>
         </form>
       </div>
 
